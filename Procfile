fb42d9a2de7388e09f09fc121e8cf9dca12ceb79
Victor4You <patovm_1998@hotmail.com>
2024-07-29 22:04:37 -0700

    servidor actualizacion

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/spring-boot-starter-parent-jar.jar
--- src/main/java/com/prueba/marvel/model/Character.java
+++ src/main/java/com/prueba/marvel/model/Character.java
@@ -7,5 +7,6 @@ public  class Character {
     private String id;
     private String name;
     private String description;
+    private String  image;
     // otros campos necesarios
 }
